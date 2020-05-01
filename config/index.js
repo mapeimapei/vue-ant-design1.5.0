afada926e66743e38cb7d6be5538c2dda0f115af
9725029@qq.com <9725029@qq.com>
2020-05-01 22:49:56 +0800

    开启https，修改打包路径

--- config/index.js
+++ config/index.js
@@ -16,7 +16,7 @@ module.exports = {
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
     port: process.env.PORT || 9666, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
-    autoOpenBrowser: false,
+    autoOpenBrowser: true,
     errorOverlay: true,
     notifyOnErrors: true,
     poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
@@ -42,9 +42,9 @@ module.exports = {
     index: path.resolve(__dirname, '../dist/templates/admin.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'vuestatic',
-    assetsPublicPath: '../',
+    assetsRoot: path.resolve(__dirname, '../dist/static'),
+    assetsSubDirectory: 'vue',
+    assetsPublicPath: '../static/',
 
     /**
      * Source Maps
